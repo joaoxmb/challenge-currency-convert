2ffb16fe730ebd22b34a56256c9b4ded9d284c64
joaoxmb <joaovictorcgdossantos@gmail.com>
2024-04-25 13:18:13 -0300

    Feat: Created simple request

---
+++ src/FetchData.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class FetchData {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/2df7a50257c7ddb5ce0fb4da/latest/BRL"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
